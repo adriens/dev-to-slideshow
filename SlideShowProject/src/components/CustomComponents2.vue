58bef77c10b5a4835097ab07e05a4059f24d8604
flavienlbrt2003 <flavien.loubriat@outlook.fr>
2023-04-27 15:21:43 +1100

    Ajout progress bar, autoplay, swip avec flèche du clavier, et diapositive n°2.

--- SlideShowProject/src/components/CustomComponents2.vue
+++ SlideShowProject/src/components/CustomComponents2.vue
@@ -1,28 +1,57 @@
 <template>
-    <div class="custom-components">
-        <p>Test2</p>
+    <div class="organization-info">
+        <h1>{{ organization.name }}</h1>
+        <p>Nom d'utilisateur : {{ organization.username }}</p>
+        <p>URL du site web : <a :href="organization.url" target="_blank">{{ organization.url }}</a></p>
+        <img :src="organization.profile_image" alt="Image de profil" />
     </div>
 </template>
-
+  
 <script>
 export default {
-    name: 'CustomComponents2',
-    /* props: {
-        // Définissez ici les propriétés que vous souhaitez passer au composant
-    }, */
-}
+    name: 'OrganizationInfo',
+    data() {
+        return {
+            organization: {}
+        };
+    },
+    async created() {
+        try {
+            const apiUrl = 'https://dev.to/api/organizations/optnc';
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            if (response.ok) {
+                this.organization = data;
+            } else {
+                console.error('Erreur lors de la récupération des données de l\'organisation :', data.error);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données de l\'organisation :', error);
+        }
+    }
+};
 </script>
   
-<style>
-.custom-components {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+<style scoped>
+.organization-info {
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
+    max-width: 600px;
+    margin: 0 auto;
+    color: black;
+}
+
+img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+h1 {
+    font-weight: bold;
 }
 </style>
   
